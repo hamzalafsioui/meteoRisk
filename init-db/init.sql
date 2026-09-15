640c930fb7827b030e94857dc930830be151c564
hamzalafsioui <hamzalafsioui@gmail.com>
2026-09-15 10:48:49 +0100

    implement docker envr

---
+++ init-db/init.sql
@@ -0,0 +1,14 @@
+
+-- Create DB
+-- CREATE DATABASE meteorisk;
+
+-- -- Connect to meteorisk
+-- \c meteorisk
+
+-- create schemas
+CREATE SCHEMA IF NOT EXISTS bronze;
+CREATE SCHEMA IF NOT EXISTS silver;
+CREATE SCHEMA IF NOT EXISTS gold;
+
+
+
